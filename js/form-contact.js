02b66ce095649f7b4e882671c3448cd4b6209a86
syonzimercy <67085208+syonzimercy@users.noreply.github.com>
2020-06-19 04:41:58 +0300

    Update form-contact.js

--- js/form-contact.js
+++ js/form-contact.js
@@ -1,35 +1,41 @@
 
-// JavaScript contact form Document
+// // JavaScript contact form Document
 
-    $('#contact-form').submit(function() {
+//     $('#contact-form').submit(function() {
 
-        var action = $(this).attr('action');
+//         var action = $(this).attr('action');
 
-        $("#message").slideUp(750, function() {
-            $('#message').hide();
+//         $("#message").slideUp(750, function() {
+//             $('#message').hide();
 
-            $('#submit')
-                .before('')
-                .attr('disabled', 'disabled');
+//             $('#submit')
+//                 .before('')
+//                 .attr('disabled', 'disabled');
 
-            $.post(action, {
-                    name: $('#name').val(),
-                    email: $('#email').val(),
-                    comments: $('#comments').val(),
-                },
-                function(data) {
-                    document.getElementById('message').innerHTML = data;
-                    $('#message').slideDown('slow');
-                    $('#cform img.contact-loader').fadeOut('slow', function() {
-                        $(this).remove()
-                    });
-                    $('#submit').removeAttr('disabled');
-                    if (data.match('success') != null) $('#cform').slideUp('slow');
-                }
-            );
+//             $.post(action, {
+//                     name: $('#name').val(),
+//                     email: $('#email').val(),
+//                     comments: $('#comments').val(),
+//                 },
+//                 function(data) {
+//                     document.getElementById('message').innerHTML = data;
+//                     $('#message').slideDown('slow');
+//                     $('#cform img.contact-loader').fadeOut('slow', function() {
+//                         $(this).remove()
+//                     });
+//                     $('#submit').removeAttr('disabled');
+//                     if (data.match('success') != null) $('#cform').slideUp('slow');
+//                 }
+//             );
 
-        });
+//         });
 
-        return false;
+//         return false;
 
-    });
+//     });
+
+$(document).ready(function(){
+  $("#contact-form").submit(function(){
+    alert("Submitted Thank you! will get back to you shortly");
+  });
+});
